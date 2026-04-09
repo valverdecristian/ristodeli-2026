0e9912ef49ee6869c5716190fb12f7ef7b6d8c5f
valverdecristian <crizthian2010@gmail.com>
2026-04-09 00:51:44 -0300

    feat(shared): implementar componente de registro dinámico y flujos de alta para admin/cliente closes #13

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,7 +15,15 @@ export const routes: Routes = [
     loadComponent: () => import('./features/auth/login/login.page').then( m => m.LoginPage)
   },
   {
+    path: 'registro-cliente',
+    loadComponent: () => import('./features/auth/registro-cliente/registro-cliente.page').then( m => m.RegistroClientePage)
+  },
+  {
     path: 'admin',
     loadComponent: () => import('./features/admin/dashboard/dashboard.page').then( m => m.DashboardPage)
+  },
+  {
+    path: 'admin/crear-empleado',
+    loadComponent: () => import('./features/admin/crear-empleado/crear-empleado.page').then( m => m.CrearEmpleadoPage)
   }
 ];
