b6cf74793d704f99c29c7c0eec8fa58ebda37afc
valverdecristian <crizthian2010@gmail.com>
2026-05-06 19:59:13 -0300

    chore: limpieza total de Angular en develop para iniciar React Native

--- src/app/core/models/notificacion.model.ts
+++
@@ -1,5 +0,0 @@
-export interface PushNotificationPayload {
-  title: string;
-  body: string;
-  data?: any;
-}
--- src/environments/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDfIUZNnANhFMtEyKeLiMRWKGpUT9lXkTo",
-  authDomain: "ristodeli.firebaseapp.com",
-  projectId: "ristodeli",
-  storageBucket: "ristodeli.firebasestorage.app",
-  messagingSenderId: "311975274322",
-  appId: "1:311975274322:android:23cc8befd5d8c1cfbe3c8b"
-};
